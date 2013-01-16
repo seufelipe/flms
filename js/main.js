4a88877957a79c15c129b4d1c5e6ee1e03911219
Mathias Kretschek <mathias@kretschek.com.br>
2013-01-16 13:07:50 -0200

    Avaliação dos filmes
    Criado método para avaliação dos filmes.
    
    Novo formato para a lista de filmes:
    rate[tab]tags[tab]título
    
    rate é opcional pode ser:
    * ++ = favorite
    * + = good
    * - = bad
    
    tags pode ser qualquer número dos seguintes valors:
    * r = review (filme está sendo revisto)

--- js/main.js
+++ js/main.js
@@ -6,10 +6,42 @@ $(function() {
     $(counterSelector).text(count);
   }
 
+  function buildTag(cls, content) {
+    return $('<em></em>').addClass(cls).append(content);
+  }
+
   function printList(list) {
     for (var i = 0, len = list.length; i < len; i++) {
-      var movieTitle = list[i];
-      $(listSelector).append($('<li></li>').text(movieTitle));
+      var data = list[i].split("\t");
+
+      if (data.length !== 3) {
+        console.log('Formato inválido na linha %s: %s', i, list[i]);
+        continue;
+      }
+
+      var rate = data[0],
+        tags=data[1],
+        title=data[2];
+
+      var movie = {};
+      var item = $('<li></li>').text(title);
+
+      switch(rate) {
+        case '++': movie.rate = 'favorite'; break;
+        case '+': movie.rate = 'good'; break;
+        case '-': movie.rate = 'bad'; break;
+      }
+
+      for (var j = 0, lenj = tags.length; j < lenj; j++) {
+        var tag = tags[j];
+        switch(tag) {
+          case 'r': movie.review = true; break;
+        }
+      }
+
+      if (movie.rate) item.addClass(movie.rate).append(buildTag('rate', movie.rate));
+      if (movie.review) item.append(buildTag('review', 'review'));
+      $(listSelector).append(item);
     }
   }
 
